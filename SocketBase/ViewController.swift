7fccb2935f51e0009779e0ea29376b4bcf96e370
Sutean Rutjanalard <mossila@icloud.com>
2016-04-29 09:50:36 +0700

    Initial Commit

---
+++ SocketBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocketBase
+//
+//  Created by Sutean Rutjanalard on 4/29/2559 BE.
+//  Copyright © 2559 Sutean Rutjanalard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
